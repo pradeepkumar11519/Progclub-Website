3b427ea5321254ebae8240dbc6a433cdca5718d8
Mukul Jain <mukul.jain1906@yahoo.com>
2023-03-09 11:31:53 +0530

    Renamed backend to server

--- server/api/admin.py
+++ server/api/admin.py
@@ -22,7 +22,7 @@ from django.contrib.admin import AdminSite
 
 class MyAdminSite(AdminSite):
     # Text to put at the end of each page's <title>.
-    site_title = "Programming Club Backend"
+    site_title = "The Programming Club Backend"
 
     # Text to put in each page's <h1> (and above login form).
     site_header = "PClub Backend"
