2e6108aed8098d2ba2d80bc84750c9891a096a62
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-03-16 00:24:48 +0530

    Added PWA functionality

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa')({
+    dest:"public",
+    register:true,
+    runtimeCaching,
+    skipWaiting:true,
+    buildExcludes: [/middleware-manifest.json$/],
+})
+const nextConfig = withPWA({
   reactStrictMode: true,
-}
+  
+})
 
 module.exports = nextConfig
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="manifest" href="/manifest.webmanifest"/>
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
