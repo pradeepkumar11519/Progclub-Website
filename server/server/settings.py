771fa4ce8b65159c0995749df128ae164e7b0035
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-03-19 19:58:12 +0530

    allowed hosts changed

--- server/server/settings.py
+++ server/server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "&%bf_82zxdb_kxj49h_92f0@o1tb0s55zxu4+qv4^o+tg7jvtk"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
