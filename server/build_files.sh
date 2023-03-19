e5dad17ddd28391c81a3c8d6d20e3bb0db595d28
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-03-19 23:41:25 +0530

    Changed build_files.sh

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,11 +1,4 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-<<<<<<< HEAD
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
-=======
-
-#Run the project
-
-echo "Make Migration..."
->>>>>>> feature
