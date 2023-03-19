9226599a0c1ec27597fb536b09b2c7af163f930f
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-03-19 20:50:09 +0530

    changed wsgi file

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -14,6 +14,6 @@ from .settings import BASE_DIR
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+
 
 app = application
