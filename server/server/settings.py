405aa88d240a9697fa672d40ba08db8c06966b74
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-03-22 11:20:15 +0530

    Reverted a commit

--- server/server/settings.py
+++ server/server/settings.py
@@ -144,7 +144,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CORS_ALLOW_ALL_ORIGINS = True
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
