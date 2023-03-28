93d5e668df751c205bdc5577c617b2d411b34a4f
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-03-28 18:59:06 +0530

    Added project title in admin

--- server/api/admin.py
+++ server/api/admin.py
@@ -36,7 +36,7 @@ class EventAdmin(admin.ModelAdmin):
 class ProjectAdmin(admin.ModelAdmin):
     model = Project
     search_fields = ("title",)
-    list_display = ("title", "image_preview")
+    list_display = ("title", "image_preview","subtitle")
     list_filter = ("domain", "category")
     readonly_fields = ("image_preview",)
 
@@ -50,7 +50,7 @@ class ProjectAdmin(admin.ModelAdmin):
             None,
             {
                 "fields": (
-                    "title",
+                    ("title","subtitle"),
                     "github",
                     ("domain", "category"),
                     "description",
